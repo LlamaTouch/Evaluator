0c622500452cd829a37d3ede9e63aa30ee437baf
Shihe Wang <wshprimy@gmail.com>
2024-02-29 23:15:13 +0800

    feat: load_groundtruth_trace

--- task_trace.py
+++ task_trace.py
@@ -18,7 +18,7 @@ class TaskCategory(Enum):
     GENERATED = "generated"
 
 
-GROUNDTRUTH_DATASET_PATH = "/data/xxx"
+GROUNDTRUTH_DATASET_PATH = "/data/yyh/mobile/capture/AITW_decode"
 
 AGENT_EXEC_TRACE_FOLDER = {
     Agents.APPAGENT: "data/agent_exec_trace/AppAgent",
@@ -28,17 +28,43 @@ AGENT_EXEC_TRACE_FOLDER = {
 }
 
 
-def load_groundtruth_trace_by_episode(episode) -> List[Tuple[Any, str, Dict]]:
+def load_groundtruth_trace(category: TaskCategory) -> Dict[str, List[Tuple[Any, str, Dict]]]:
     """
-    *TODO*
-    Return: [[screenshot1, XML1, action1], [screenshot2, XML2, action2], ...]
+    Return: {
+        "episode_id_1": [(screenshot_1_1, XML_1_1, action_1_1), (screenshot_1_2, XML_1_2, action_1_2), ...],
+        "episode_id_2": [(screenshot_2_1, XML_2_1, action_2_1), (screenshot_2_2, XML_2_2, action_2_2), ...],
+        ...
+    }
     """
-    category = DatasetHelper().get_category_by_episode(episode)
-    groundtruth_trace_folder = os.path.join(
-        GROUNDTRUTH_DATASET_PATH, category, episode, "captured_data"
-    )
-    return load_trace(groundtruth_trace_folder)
+    groundtruth_trace_folder = os.path.join(GROUNDTRUTH_DATASET_PATH, category.value)
+    gt_trace_dict = {}
+    dirs = [d for d in os.listdir(groundtruth_trace_folder) if os.path.isdir(os.path.join(groundtruth_trace_folder, d))]
+    dirs.sort()
+    for dir in dirs:
+        path = os.path.join(groundtruth_trace_folder, dir)
+        ep_trace_list = _gt_trace_episode(path)
 
+        ep_id = dir # TODO: get ep_id
+        gt_trace_dict[ep_id] = ep_trace_list
+
+    return gt_trace_dict
+
+def _gt_trace_episode(path: str) -> List[Tuple[Any, str, Dict]]:
+    ep_trace_list = []
+    files = [f for f in os.listdir(path) if f.endswith(".png")]
+    files.sort()
+
+    action_path = os.path.join(path, "eventStructs.txt")
+    for file in files:
+        img_path = os.path.join(path, file)
+        xml_path = os.path.join(path, file.replace("png_image.png", "png_xml.txt"))
+
+        # TODO: get action, img, xml
+        action = None
+        ep_trace_list.append((img_path, xml_path, action))
+        # print(img_path)
+    
+    return ep_trace_list
 
 def get_agent_exec_trace_folder(agent_name, episode) -> str:
     """Get the folder of agent execution trace for one specific episode"""
@@ -68,14 +94,6 @@ def load_agent_exec_trace_by_episode(
 ) -> List[Tuple[Any, str, Dict]]:
     agent_exec_trace_folder = get_agent_exec_trace_folder(agent_name, episode)
     return load_trace(agent_exec_trace_folder)
-
-
-def load_groundtruth_trace_by_episode(episode) -> List[Tuple[Any, str, Dict]]:
-    category = DatasetHelper().get_category_by_episode(episode)
-    groundtruth_trace_folder = os.path.join(
-        GROUNDTRUTH_DATASET_PATH, category, episode, "captured_data"
-    )
-    return load_trace(groundtruth_trace_folder)
 
 
 def load_trace(trace_folder) -> List[Tuple[Any, str, Dict]]:
@@ -133,3 +151,7 @@ class DatasetHelper:
 
     def get_category_by_episode(self, episode) -> TaskCategory:
         return self.epi_metadata_dict[episode]["category"]
+
+if __name__ == '__main__':
+    gt_trace = load_groundtruth_trace(TaskCategory.GENERAL)
+    print(gt_trace)
