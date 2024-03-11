862b4b444bda678896c181973c4f3846d5c683c9
piaoliangkb <piaoliangkb@gmail.com>
2024-03-11 10:48:35 +0800

    up setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from setuptools import find_packages, setup
 setup(
     name="matestbed",
     version="0.0.1",
-    packages=find_packages(exclude=["evaluator.test"]),
+    packages=find_packages(include=["evaluator.*"]),
     install_requires=["numpy", "pandas", "lxml"],
-    # Consider including this if you have a readme that's useful for developers or users within your organization
-    long_description=open("README.md").read() if os.path.exists("README.md") else "",
 )
