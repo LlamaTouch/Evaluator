8b740d6edbef5b126eb0abff5f6cb4e69a2a8826
piaoliangkb <piaoliangkb@users.noreply.github.com>
2024-03-11 01:44:37 +0000

    Apply black formatter

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ import os
 from setuptools import find_packages, setup
 
 setup(
-    name='MATestbed',
-    version='0.0.1',
+    name="MATestbed",
+    version="0.0.1",
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'lxml'
-    ],
+    install_requires=["numpy", "pandas", "lxml"],
     # Consider including this if you have a readme that's useful for developers or users within your organization
-    long_description=open('README.md').read() if os.path.exists('README.md') else "",
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
 )
