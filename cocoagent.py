c1168b30565f3d8cdf22952582252562062f1228
piaoliangkb <piaoliangkb@gmail.com>
2024-04-02 20:58:25 +0800

    feat: add cocoagent impl

--- cocoagent.py
+++ cocoagent.py
@@ -1,6 +1,4 @@
 import argparse
-import ast
-import json
 import os
 from typing import Dict, List, Optional
 
@@ -12,41 +10,15 @@ from evaluator.task_trace import Agent, DatasetHelper, TaskCategory, TaskTrace
 from evaluator.testbed_evaluator import TestbedEvaluator
 
 
-class AutoUI(MobileAgent):
+class CoCoAgent(MobileAgent):
+
     def __init__(self) -> None:
         super().__init__()
-        self.agent = Agent.AUTOUI
-        self.agent_exec_trace_path = (
-            "/data/zzh/mobile-agent/Auto-UI/agentenv/agent_result"
+        self.agent = Agent.COCOAGENT
+        self.epi_to_exec_trace_path = {}
+        self.base_folder = (
+            "/data/zzh/mobile-agent/CoCoAgent/agentenv/agent_result/all_data"
         )
-        self.epi_to_action_list: Dict[str, List[Action]] = {}
-
-    def load_predicted_action_by_episode(self, episode: str) -> List[Dict] | None:
-        """TODO: implementation required."""
-        pass
-
-    # def load_predicted_action_by_episode(self, episode: str) -> Optional[List[Action]]:
-    #     # check if the action list is already loaded
-    #     if episode in self.epi_to_action_list.keys():
-    #         return self.epi_to_action_list[episode]
-
-    #     eval_result_file = "/data/zzh/mobile-agent/Auto-UI/Evaluator/result/auto_ui_evaluator_last_screen.json"
-    #     with open(eval_result_file) as f:
-    #         data = json.load(f)
-
-    #     for epi_data in data:
-    #         act_list: List[Action] = []
-    #         for action in epi_data["actions"]:
-    #             act = Action(
-    #                 action_type=ActionType[action["action_type"].upper()],
-    #                 touch_point_yx=tuple(ast.literal_eval(action["touch_point"])),
-    #                 lift_point_yx=tuple(ast.literal_eval(action["lift_point"])),
-    #                 typed_text=action["typed_text"],
-    #             )
-    #             act_list.append(act)
-    #         self.epi_to_action_list[epi_data["episode_id"]] = act_list
-
-    #     return self.epi_to_action_list.get(episode)
 
     def load_predicted_action_by_episode(self, episode: str) -> Optional[List[Action]]:
         exec_trace: TaskTrace = self.load_exec_trace_by_episode(episode)
@@ -55,33 +27,24 @@ class AutoUI(MobileAgent):
         return None
 
     def load_exec_trace_by_episode(self, episode: str) -> Optional[TaskTrace]:
-        category = DatasetHelper().get_category_by_episode(episode)
-        if category == TaskCategory.WEBSHOPPING:
-            category_val = "web_shopping"
-        elif category == TaskCategory.GOOGLEAPPS:
-            category_val = "google_apps"
-        else:
-            category_val = category.value
-        epi_trace_path = os.path.join(
-            self.agent_exec_trace_path, category_val, episode, "captured_data"
-        )
-        if not os.path.exists(epi_trace_path):
+        epi_folder = os.path.join(self.base_folder, episode)
+        if not os.path.exists(epi_folder):
             return None
-        return DatasetHelper().load_testbed_trace_by_path(epi_trace_path)
+
+        return DatasetHelper().load_testbed_trace_by_path(epi_folder)
 
     def load_exec_trace_path_by_episode(self, episode: str) -> str:
         pass
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Run AutoUI evaluation.")
+    parser = argparse.ArgumentParser(description="Run CoCo-Agent evaluation.")
     parser.add_argument(
         "--eval", type=str, help='Evaluation type: "testbed (t)" or "exact (e)"'
     )
     args = parser.parse_args()
 
-    agent = AutoUI()
+    agent = CoCoAgent()
 
     if args.eval == "exact" or args.eval == "e":
         e = ExactMatchEvaluator(
@@ -117,6 +80,7 @@ if __name__ == "__main__":
         )
         t.run_evaluation()
         t.report_stats()
+
     elif args.eval == "lcs-exact" or args.eval == "lcse":
         t = LCSMatchEvaluator(
             agent=agent,
@@ -132,6 +96,7 @@ if __name__ == "__main__":
         )
         t.run_evaluation()
         t.report_stats()
+
     else:
         raise Exception(
             f"Invalid evaluation type: {args.eval}, expected: testbed/t and exact/e"
