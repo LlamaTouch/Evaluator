d1e1a20643ea3e0e1f4344cedbfb02989f9539c6
Shihe Wang <wshprimy@gmail.com>
2024-04-03 09:41:17 +0800

    feat: FLAG only_human_eval_positive

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -84,27 +84,37 @@ class BaseEvaluator(ABC):
         pass
 
     def report_stats(
-        self, human_eval_path: str = None, to_stdout: bool = False
+        self,
+        human_eval_path: str = None,
+        only_human_eval_positive: bool = False,
+        to_stdout: bool = False,
     ) -> None:
         exec_dict = {
             epi: completed for epi, (completed, _) in self.episode_completion.items()
         }
         exec_df = pd.DataFrame(exec_dict.items(), columns=["episode", "execution"])
-        exec_true = (exec_df["execution"] == 1).sum()
-        exec_false = (exec_df["execution"] == 0).sum()
-        print(f"Completed tasks: {exec_true}, failed tasks: {exec_false}")
-
         if not human_eval_path:
+            exec_positive = (exec_df["execution"] == 1).sum()
+            exec_negative = (exec_df["execution"] == 0).sum()
+            print(f"Completed tasks: {exec_positive}, failed tasks: {exec_negative}")
             self._dump_stats(to_stdout=to_stdout)
         else:
             with open(human_eval_path, "r") as f:
                 human_df = pd.read_csv(f)
             eval_df = human_df.merge(exec_df, on="episode", how="inner")
+            if only_human_eval_positive:
+                eval_df = eval_df[eval_df[eval_df.columns[1]] == 1]
             total = eval_df.shape[0]
-            tp = (eval_df[eval_df.columns[1]] == eval_df["execution"]).sum()
+            if only_human_eval_positive and total < 1:
+                total = 1
 
             human_tcr = (eval_df[eval_df.columns[1]] == 1).sum() / total
-            tcr = exec_true / total
+            exec_positive = (exec_df["execution"] == 1).sum()
+            exec_negative = total - exec_positive
+            print(f"Completed tasks: {exec_positive}, failed tasks: {exec_negative}")
+            tcr = exec_positive / total
+
+            tp = (eval_df[eval_df.columns[1]] == eval_df["execution"]).sum()
             acc = tp / total
             self._dump_stats(metric=(human_tcr, tcr, acc), to_stdout=to_stdout)
 
