aa09d21e45f8d599199a27e189859519f93c3fc5
xianqing Jia <149289310+xianqingjia777@users.noreply.github.com>
2024-05-21 17:02:04 +0800

    feat: add image exact match; fix click exact match (#3)
    * add image_exact_match and modify click_exact_match logit
    
    * add click_match and img_match test module
    
    * add imagehash dependency
    
    * update test module
    
    * fix img_exact_match and remove IMAGE
    
    * modify img_exact_match
    
    * remove commented-out code
    
    * modify img_match_test
    
    * fix null_state

--- evaluator/testbed_evaluation/exact_match.py
+++ evaluator/testbed_evaluation/exact_match.py
@@ -1,16 +1,65 @@
 import json
 import re
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from lxml import etree
 from PIL import Image
+import imagehash
 
 from ..common.action_type import ActionType
 from ..task_trace import EssentialStateKeyword, UIState
 
 
-def check_img_match(gr_ui_state: UIState, exec_ui_state: UIState) -> bool:
-    pass
+def _get_image_patch(image: Image, bounds: List[int]) -> Image:
+    left, top, right, bottom = bounds
+    return image.crop((left, top, right, bottom))
+
+def _check_img_exact_match(
+    annotated_ui_node: Dict, gr_screenshot_path: str, exec_screenshot_path: str,
+    image_similarity_bound: Optional[int] = 1
+)->bool:
+    """
+    Compare whether the image patch of the annotated UI component matches 
+    the corresponding image patch in the execution trace.
+
+    Args:
+        annotated_ui_node: annotated essential state that represents a UI node
+        gr_screenshot_path: screenshot path of the annotated UI 
+        exec_screenshot_path: screenshot path of the execution UI
+        image_similarity_bound: threshold to determine whether the image patches are similar
+    
+    Return:
+        boolean value indicating whether the image patch of the annotated UI component 
+        and the execution UI component matches
+    """
+    gr_bounds = annotated_ui_node["bounds"]
+    assert gr_bounds is not None
+
+    gr_screen_width, gr_screen_height = 0,0
+    exec_screen_width, exec_screen_height = 0,0
+    with Image.open(gr_screenshot_path) as img:
+        gr_screen_width, gr_screen_height = img.size
+    with Image.open(exec_screenshot_path) as img:
+        exec_screen_width, exec_screen_height = img.size
+
+    gr_l, gr_t, gr_r, gr_b =  map(int, re.findall(r"\[(\d+),(\d+)\]\[(\d+),(\d+)\]", gr_bounds)[0])
+    exec_l, exec_t, exec_r, exec_b =  gr_l * exec_screen_width / gr_screen_width, gr_t * exec_screen_height / gr_screen_height,\
+            gr_r * exec_screen_width / gr_screen_width, gr_b * exec_screen_height / gr_screen_height
+    
+    annotate_image_patch: Image = _get_image_patch(Image.open(gr_screenshot_path), [gr_l, gr_t, gr_r, gr_b])
+    exec_image_patch: Image = _get_image_patch(Image.open(exec_screenshot_path), [exec_l, exec_t, exec_r, exec_b])   
+
+    gr_hash = imagehash.average_hash(annotate_image_patch)
+    exec_hash = imagehash.average_hash(exec_image_patch)
+
+    if gr_hash - exec_hash > image_similarity_bound:
+        print(
+            f"[image] match fail: hamming distance: {gr_hash-exec_hash}, '{gr_screenshot_path}' with '{exec_screenshot_path}'"
+        )
+        return False
+    
+    print(f"[image] match success: hamming distance: {gr_hash-exec_hash}, '{gr_screenshot_path}' with '{exec_screenshot_path}'")
+    return True
 
 
 def _check_exact_single_node_match(
@@ -89,8 +138,9 @@ def _check_exact_single_node_match(
 def check_uicomponent_match(gr_ui_state: UIState, exec_ui_state: UIState) -> bool:
     """Exact match on two UI components"""
     match_node_ids: List[str] = gr_ui_state.essential_state[
-        EssentialStateKeyword.TEXTBOX
+        EssentialStateKeyword.EXACT
     ]
+    null_state = [""," ","null",None]
 
     parser = etree.XMLParser(recover=True, encoding="utf-8")
     exec_ui_tree = etree.parse(exec_ui_state.vh_path, parser)
@@ -102,11 +152,18 @@ def check_uicomponent_match(gr_ui_state: UIState, exec_ui_state: UIState) -> boo
             open(gr_vh_simp_ui_json_path, "r", encoding="utf-8")
         )[node_id]
 
-        # if there is one annotated UI component (indicated by node_id) has no
-        # matched counterpart, directly return False to indicate
-        if not _check_exact_single_node_match(annotated_ui_repr, exec_ui_tree):
-            # print(f"[textbox] match failed: '{gr_ui_state.screenshot_path}', essential state: {annotated_ui_repr}")
-            return False
+        if annotated_ui_repr.get("text",None) in null_state and \
+            annotated_ui_repr.get("content-desc",None) in null_state:
+             if not _check_img_exact_match(annotated_ui_repr,gr_ui_state.screenshot_path,\
+                                           exec_ui_state.screenshot_path):
+                 return False
+        
+        else:
+            # if there is one annotated UI component (indicated by node_id) has no
+            # matched counterpart, directly return False to indicate
+            if not _check_exact_single_node_match(annotated_ui_repr, exec_ui_tree):
+                # print(f"[textbox] match failed: '{gr_ui_state.screenshot_path}', essential state: {annotated_ui_repr}")
+                return False
 
     print(
         f"[textbox] match success: '{gr_ui_state.screenshot_path}' with '{exec_ui_state.screenshot_path}'"
@@ -141,56 +198,42 @@ def check_type_match(gr_ui_state: UIState, exec_ui_state: UIState) -> bool:
 
 
 def check_click_match(gr_ui_state: UIState, exec_ui_state: UIState) -> bool:
-    """TODO: this implementation can be optimized"""
+    """
+    based on the clicked item xpath in gr_ui_state.essential_state, find 
+    the corresponding node in the exec_ui_state and check if the click point in the node.
+    """
+
     if exec_ui_state.action.action_type != ActionType.DUAL_POINT:
         return False
 
     if exec_ui_state.action.touch_point_yx != exec_ui_state.action.lift_point_yx:
         return False
 
-    # based on exec_ui_state.action.touch_point_yx, find the corresponding node
-    # in the XML
-    screen_width, screen_height = Image.open(exec_ui_state.screenshot_path).size
-    y: float = exec_ui_state.action.touch_point_yx[0] * screen_height
-    x: float = exec_ui_state.action.touch_point_yx[1] * screen_width
-
+    gr_clicked_item_xpath = str(gr_ui_state.essential_state[EssentialStateKeyword.CLICK][0])
+    
     parser = etree.XMLParser(recover=True, encoding="utf-8")
     exec_ui_tree = etree.parse(exec_ui_state.vh_path, parser)
 
-    smallest_node = None
+    found_nodes = exec_ui_tree.xpath(gr_clicked_item_xpath)
+    if len(found_nodes) == 0:
+        raise AssertionError("No corresponding nodes found in the execution UI state.")
 
-    for node in exec_ui_tree.iter():
-        bounds = node.get("bounds")
-        if bounds is None:
-            continue
-        left, top, right, bottom = map(
-            float, re.findall(r"\[(\d+),(\d+)\]\[(\d+),(\d+)\]", bounds)[0]
-        )
-        if left <= x <= right and top <= y <= bottom:
-            area = (right - left) * (bottom - top)
+    bounds = found_nodes[0].get("bounds")
+    if not bounds:
+        raise AssertionError("No bounds found for the corresponding node.")
+    left, top, right, bottom = map(int, re.findall(r"\[(\d+),(\d+)\]\[(\d+),(\d+)\]", bounds)[0])
+    
+    screen_width, screen_height = 0,0
+    with Image.open(exec_ui_state.screenshot_path) as img:
+        screen_width, screen_height = img.size
 
-            if smallest_node is None or area < smallest_area:
-                smallest_node = node
-                smallest_area = area
+    # screen_width, screen_height = Image.open(exec_ui_state.screenshot_path).size
+    y = exec_ui_state.action.touch_point_yx[0] * screen_height
+    x = exec_ui_state.action.touch_point_yx[1] * screen_width
 
-    # there will be only one click action on the screen
-    gr_vh_simp_ui_json_path = gr_ui_state.vh_simp_ui_json_path
-    node_id: int = int(gr_ui_state.essential_state[EssentialStateKeyword.CLICK][0])
-    annotated_ui_repr: Dict = json.load(
-        open(gr_vh_simp_ui_json_path, "r", encoding="utf-8")
-    )[node_id]
-    target_class = annotated_ui_repr["class"]
-    target_text = annotated_ui_repr["text"]
-    target_resource_id = annotated_ui_repr["resource-id"]
-
-    if (
-        smallest_node.get("class") == target_class
-        and smallest_node.get("text") == target_text
-        and smallest_node.get("resource-id") == target_resource_id
-    ):
-        print(
-            f"[click] match success: '{gr_ui_state.screenshot_path}' with '{exec_ui_state.screenshot_path}'"
-        )
+    if left <= x <= right and top <= y <= bottom:
+        print(f"[click] match success: click action:{x,y}, '{gr_ui_state.vh_path}' with '{exec_ui_state.vh_path}'")
         return True
     else:
+        print(f"[click] match failed: click action:{x,y}, '{gr_ui_state.vh_path}' with '{exec_ui_state.vh_path}'")  
         return False
---
+++ evaluator/testbed_evaluation/tests/img_match_test.py
@@ -0,0 +1,189 @@
+import unittest
+from evaluator.task_trace import EssentialStateKeyword
+from evaluator.testbed_evaluation.exact_match import _check_img_exact_match
+import json
+
+class TestCheckImgMatch(unittest.TestCase):
+    def setUp(self):
+        self.case_json = [
+             "evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.json",
+             "evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.json",
+             "evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.json",
+        ]
+        self.test_data = [
+            # """small size of EXACT"""
+            {   
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "image_similarity_bounds": 1,
+                "expected": True  
+            },
+            {   
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "image_similarity_bounds": 5,
+                "expected": True  
+            },
+            {   
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "image_similarity_bounds": 10,
+                "expected": True  
+            },
+            {   
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/3.png',
+                "image_similarity_bounds": 1,
+                "expected": True
+            },
+            {   
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/3.png',
+                "image_similarity_bounds": 5,
+                "expected": True
+            },
+            {   
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/3.png',
+                "image_similarity_bounds": 10,
+                "expected": True
+            },
+            {   
+                # EXACT don`t match 
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/0.png',
+                "image_similarity_bounds": 1,                
+                "expected": False
+            },
+            {   
+                # EXACT don`t match 
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/0.png',
+                "image_similarity_bounds": 5,                
+                "expected": False
+            },
+            {   
+                # EXACT don`t match 
+                "annotated_ui_node": json.load(open(self.case_json[0], "r", encoding="utf-8"))[0],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/2.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case1/0.png',
+                "image_similarity_bounds": 10,                
+                "expected": False
+            },
+
+            # """middle size of EXACT"""
+            {
+                "annotated_ui_node": json.load(open(self.case_json[1], "r", encoding="utf-8"))[16],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "image_similarity_bounds": 1,
+                "expected": True
+            },
+            {
+                "annotated_ui_node": json.load(open(self.case_json[1], "r", encoding="utf-8"))[16],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "image_similarity_bounds": 5,
+                "expected": True
+            },
+            {
+                "annotated_ui_node": json.load(open(self.case_json[1], "r", encoding="utf-8"))[16],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "image_similarity_bounds": 10,
+                "expected": True
+            },
+            {
+                # EXACT don`t match
+                "annotated_ui_node": json.load(open(self.case_json[1], "r", encoding="utf-8"))[16],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/2.png',
+                "image_similarity_bounds": 1,
+                "expected": False
+            },
+            {
+                # EXACT don`t match
+                "annotated_ui_node": json.load(open(self.case_json[1], "r", encoding="utf-8"))[16],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/2.png',
+                "image_similarity_bounds": 5,
+                "expected": True
+            },
+            {
+                # EXACT don`t match
+                "annotated_ui_node": json.load(open(self.case_json[1], "r", encoding="utf-8"))[16],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case2/2.png',
+                "image_similarity_bounds": 10,
+                "expected": True
+            },
+            # """large size of EXACT"""
+            {
+                "annotated_ui_node": json.load(open(self.case_json[2], "r", encoding="utf-8"))[22],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "image_similarity_bounds": 1,
+                "expected": True
+            },
+            {
+                "annotated_ui_node": json.load(open(self.case_json[2], "r", encoding="utf-8"))[22],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "image_similarity_bounds": 5,
+                "expected": True
+            },
+            {
+                "annotated_ui_node": json.load(open(self.case_json[2], "r", encoding="utf-8"))[22],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "image_similarity_bounds": 10,
+                "expected": True
+            },
+            {
+                # EXACT don`t match
+                "annotated_ui_node": json.load(open(self.case_json[2], "r", encoding="utf-8"))[22],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/2.png',
+                "image_similarity_bounds": 1,
+                "expected": False
+            },
+            {
+                # EXACT don`t match
+                "annotated_ui_node": json.load(open(self.case_json[2], "r", encoding="utf-8"))[22],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/2.png',
+                "image_similarity_bounds": 5,
+                "expected": False
+            },
+            {
+                # EXACT don`t match
+                "annotated_ui_node": json.load(open(self.case_json[2], "r", encoding="utf-8"))[22],
+                "gr_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/1.png',
+                "exec_image_path": 'evaluator/testbed_evaluation/tests/test_case/img_test_case/case3/2.png',
+                "image_similarity_bounds": 10,
+                "expected": False
+            },
+            
+        ]
+
+    def test_check_img_match(self):
+        for test_case in self.test_data:
+                with self.subTest(test_case=test_case):
+                    bound = test_case["image_similarity_bounds"]
+                    annotated_ui_node = test_case["annotated_ui_node"]
+                    gr_screenshot_path = test_case["gr_image_path"]
+                    exec_screenshot_path = test_case["exec_image_path"]
+                    result = _check_img_exact_match(annotated_ui_node,gr_screenshot_path, exec_screenshot_path, bound)
+                    print(f"Testing with bound {bound} and paths {test_case['gr_image_path']} vs {test_case['exec_image_path']}, Result: {result}")
+                    self.assertEqual(result, test_case["expected"])
+
+if __name__ == '__main__':
+    unittest.main()
